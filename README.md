34deb2cc20e6aa0b5f5ae58ad1540f574a16a78a
Casey D <caseydasilva@hotmail.com>
2020-11-10 11:23:45 -0800

    'Post' and 'get' requests
    working with diff requests to form the 'sign up'/'sign in' pages of the website/app. making sure the requests are properly submitted and figuring out how to access the FORM DATA within the console network. also using outside libraries to make the work easier

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-eComm
+* eComm App
+
+- create a simple eCommerce App with multiple linked pages and basic functions of online shopping (selecting, adding-to-cart, check out, etc)
--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-    res.send('hi there!')
+    res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <input name="confirmPassword" placeholder="confirm password" />
+            <button>Sign Up</button>
+        </form>
+    </div>
+    `)
+});
+
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('Account created!!')
 });
 
 app.listen(3000, () => {
