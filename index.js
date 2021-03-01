07ff2eb231b1cc3a1bbef2353f23206a1ea855b0
Casey D <caseydasilva@hotmail.com>
2021-03-01 12:14:55 -0800

    created Users Repo
    Users Repo for app in order to store records of all user info (email,pass, & unique ID for each user)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const usersRepo = require('./repositories/users');
 
 const app = express();
 
@@ -8,22 +9,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.send(`
     <div>
-        <form method="POST">
-            <input name="email" placeholder="email" />
-            <input name="password" placeholder="password" />
-            <input name="confirmPassword" placeholder="confirm password" />
-            <button>Sign Up</button>
-        </form>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="password confirmation" />
+        <button>Sign Up</button>
+      </form>
     </div>
-    `)
+  `);
 });
 
+app.post('/', async (req, res) => {
+    const { email, password, passwordConfirmation } = req.body;
 
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.send('Account created!!')
+    const existingUser = await usersRepo.getOneBy({ email });
+    if (existingUser) {
+        return res.send('Email in use');
+    }
+
+    if (password !== passwordConfirmation) {
+        return res.send('Passwords must match');
+    }
+
+    res.send('Account created!!!');
 });
 
 app.listen(3000, () => {
-    console.log('listening')
+    console.log('Listening');
 });
